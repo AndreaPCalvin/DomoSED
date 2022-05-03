c1c46332a0e75aad3e55aa5facc917a4572bc297
AndreaPCalvin <72571601+AndreaPCalvin@users.noreply.github.com>
2022-05-03 20:07:26 +0200

    Buzzer sound after the door is completely opened

--- placa1_puerta.ino
+++ placa1_puerta.ino
@@ -6,11 +6,13 @@
 #define SERVO_PIN 9 // Servo para puerta abierta o cerrada
 #define PULSADOR_PIN 3 // Timbre de la puerta
 #define BUZZER_PIN 12 // Sonido para visitante
+#define TIME_DEBOUNCE 200 // Tiempo (en ms) de eliminacion de rebotes
 Servo servo;
 
 boolean timbre = false;
 boolean abrirPuerta = false;
 boolean puertaAbierta = false;
+long startTime = 0;
 
 
 template <typename T> int I2C_writeAnything (const T& value)
@@ -51,32 +53,28 @@ void loop() {
    * el Timer 1 lo utiliza la biblioteca de servos y la necesitamos,
    * el Timer 2 lo utiliza Tone que lo necesitamos. Medir el
    * ultrasonidos es el único cómputo de esta placa y con este
-   * HW precario poco más se puede hacer. (Usamos timers en otras placas)
+   * HW poco más se puede hacer. (Usamos timers en otras placas)
    */
    ultrasonidos();
-   //boton();
    delay(200);
    compruebaYEnviaTimbre();
 }
 
 void ultrasonidos(){
-  // Si la distancia es pequeña aviso de una presencia en la puerta
+  // Medimos la distancia a la puerta
   long myDist = distancia();
+  // Si la distancia es suficientemente pequeña
   if(myDist < 10){
     Serial.print("Hay una persona en la puerta.");
     Serial.print(myDist);
     Serial.println("cm." );
-    Wire.beginTransmission(2);
-    // Le indico quien soy para que sepa tratar mi mensaje
-    I2C_writeAnything('1');
-    // Enviamos una 'P' de persona detectada a la placa 2 (centro de control)
-    I2C_writeAnything('P');
-    Wire.endTransmission();
+    // Informamos al control de que hay una persona delante de la puerta
+    enviaInfoACentroControl('P');
     compruebaYEnviaTimbre();
-    // No estoy enviando todo el rato la misma deteccion de persona
     /* Mientras la persona siga en la puerta simplemente espero
      * a que me indiquen desde control si abrirle la puerta o
-     * si la persona que está delante de ella toca el timbre
+     * si la persona que está delante de ella toca el timbre.
+     * Con ello no enviamos todo el rato la misma deteccion de persona
     */
     while(distancia() < 10){
       compruebaYEnviaTimbre();
@@ -85,13 +83,9 @@ void ultrasonidos(){
     }
     delay(200);
     // Informamos al control que la persona ya no está en la puerta
-    Wire.beginTransmission(2);
-    // Le indico quien soy para que sepa tratar mi mensaje
-    I2C_writeAnything('1');
-    // Enviamos una 'n' de no persona a la placa 2 (centro de control)
-    I2C_writeAnything('N');
-    Wire.endTransmission();
+    enviaInfoACentroControl('N');
   }
+  // 
   else{
     Serial.print("No hay nadie aquí.");
     Serial.print(myDist);
@@ -99,31 +93,39 @@ void ultrasonidos(){
     /* Si nos mandaron abrir la puerta y no hay nadie en
      * ella limpiamos el flag 'abrirPuerta' para no 
      * abrirle a la proxima persona que aparezca automáticamente.
+     * Lo mismo con el timbre.
      */
     abrirPuerta = false;
+    // Comprobamos si la puerta está abierta, para cerrarla
     compruebaAbiertaYCierra();
   }
 }
+
+void enviaInfoACentroControl(char info){
+    // Enviamos un mensaje al control (placa 2)
+    Wire.beginTransmission(2);
+    // Le indico quien soy para que sepa tratar mi mensaje
+    I2C_writeAnything('1');
+    // Enviamos el caracter con la información
+    I2C_writeAnything(info);
+    Wire.endTransmission();
+}
+
 
 void botonISR(){
   timbre = true;
 }
 
 void compruebaYEnviaTimbre(){
-      if(timbre) {
+   if(timbre && distancia() < 10) {
       enviaTimbre();
-      timbre = false;
    }
+   timbre = false;
 }
 
 void enviaTimbre(){
-  Wire.beginTransmission(2);
-  // Le indico quien soy para que sepa tratar mi mensaje
-  I2C_writeAnything('1');
-  // Enviamos una 'T' de timbre a la placa 2 (centro de control)
-  I2C_writeAnything('T');
-  Wire.endTransmission();
-  while(digitalRead(PULSADOR_PIN) == HIGH);
+  // Indicamos al centro de control que han tocado el timbre
+  enviaInfoACentroControl('T');
   Serial.println("Botón pulsado");
 }
 
@@ -138,25 +140,39 @@ void receiveEvent(int howMany){
 }
 
 void compruebaSiAbrirPuerta(){
+  // Si está a activado el flag que indica que hay que abrir la puerta
   if(abrirPuerta){
+    // Desactivamos el flag
     abrirPuerta = false;
+    // Siempre que siga habiendo presencia delante de la puerta
     if(distancia() < 10) {
       Serial.println("Abriendo puerta");
+      // Abrimos la puerta
       servo.write(180);
       puertaAbierta = true;
     }
+    // Esperamos tiempor suficiente para que la puerta esté abierta
+    delay(700);
+    // Emitimos un sonido para que las personas ciegas sepan que puedan pasar ya
+    pitidoCiegos();
   }
 }
 
-void compruebaAbiertaYCierra(){
-  if(puertaAbierta){
-    puertaAbierta = false;
-    servo.write(0);
-    delay(700);
-    for (int i = 0; i < 1500; i+= 1500){
+void pitidoCiegos(){
+   for (int i = 0; i < 1500; i+= 1500){
       tone(BUZZER_PIN,800-i,600);
       delay(400);
-    }
+   }
+}
+
+// Se llama a esta función sólo si ya no hay presencia delante de la puerta
+void compruebaAbiertaYCierra(){
+  // Si la puerta está abierta
+  if(puertaAbierta){
+    // Cerramos la puerta
+    servo.write(0);
+    // Actualizamos el flag para indicar que queda cerrada
+    puertaAbierta = false;
   }
 }
 
